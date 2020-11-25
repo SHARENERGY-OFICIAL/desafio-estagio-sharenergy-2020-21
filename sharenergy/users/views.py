1caf5837227d1fedd36d6613a69e4801279ca053
bieelspm <gabrielgbsspm@hotmail.com>
2020-11-25 15:23:02 -0300

    arquivo adicionado

--- sharenergy/users/views.py
+++ sharenergy/users/views.py
@@ -4,18 +4,17 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 # Create your views here.
 def list_clientes(request):
-    #clientes = get_list_or_404(Cliente)
-    part = Participacao.objects.all()
-    return render(request, 'users/list_clientes.html', {'pts': part})
+    clientes = get_list_or_404(Cliente)
+    return render(request, 'users/list_clientes.html', {'clientes':clientes})
 
 def show_cliente(request, id):
     pt = get_object_or_404(Participacao, cliente=id)
     cliente = get_object_or_404(Cliente, numero_cliente=id)
     usinas = cliente.usinas.all()
     user = {
-        'nome': cliente.nome_cliente,
+        'nome': cliente.nome_cliente.capitalize(),
         'id': cliente.numero_cliente,
-        'usinas': usinas,
+        'usinas': usinas[0],
         'porcentagem': pt.porcentagem
     }
     return render(request, 'users/show_cliente.html', user)
