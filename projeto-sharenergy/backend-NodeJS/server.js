34fd6f7ab6fcc978acd0ccbf6c351f8dbefab4d4
Daniel Hermenegildo <danielmarcosh@gmail.com>
2020-11-28 12:44:53 -0300

    Adicao da biblioteca 'require-dir' + 'alteracoes no server.js'
    Adicao do primeiro dado no BD

--- projeto-sharenergy/backend-NodeJS/server.js
+++ projeto-sharenergy/backend-NodeJS/server.js
@@ -1,19 +1,35 @@
-const express = require('express');
-const app = express();
+const express = require("express");
 
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+const app = express();
 
 // Iniciando o DB
 mongoose.connect(
-    'mongodb+srv://adm:asdf-456@cluster0.ixajh.mongodb.net/sharenergy-project-db?retryWrites=true&w=majority',
-    {useNewUrlParser: true, useUnifiedTopology: true}
+  "mongodb+srv://adm:asdf-456@cluster0.ixajh.mongodb.net/sharenergy-project-db?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+requireDir("./src/models");
 
-app.listen(3100, () =>{
-    console.log("Servidor rodando na porta 3100");
+const Cliente = mongoose.model("Cliente");
+
+app.get("/", (req, res) => {
+  Cliente.create({
+    numeroCliente: 1,
+    nomeCliente: "Ana Silva",
+    usinas: [
+      {
+        numeroUsina: 1,
+        percentualUsina: 30,
+      },
+    ],
+  });
+
+  return res.send("Olá Sharenergy");
 });
 
-app.get('/', (req, res) => {
-    res.send("Olá Sharenergy");
+app.listen(3100, () => {
+  console.log("Servidor rodando na porta 3100");
 });
