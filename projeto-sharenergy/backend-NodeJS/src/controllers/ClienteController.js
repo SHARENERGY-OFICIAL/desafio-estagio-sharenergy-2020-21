6705cba9cd982ae62ca89a51f526412a22ebffae
Daniel Hermenegildo <danielmarcosh@gmail.com>
2020-11-28 13:33:22 -0300

    Buscar Cliente por 'ID" no BD

--- projeto-sharenergy/backend-NodeJS/src/controllers/ClienteController.js
+++ projeto-sharenergy/backend-NodeJS/src/controllers/ClienteController.js
@@ -3,24 +3,34 @@ const mongoose = require("mongoose");
 const Cliente = mongoose.model("Cliente");
 
 module.exports = {
-    /**
-     * Listar todos os Clientes no BD
-     * @param {*} req 
-     * @param {*} res 
-     */
+  /**
+   * Listar todos os Clientes no BD
+   * @param {*} req
+   * @param {*} res
+   */
   async listarTodosClientes(req, res) {
     const clientes = await Cliente.find();
 
     return res.json(clientes);
   },
   /**
+   * Buscar Cliente no BD
+   * @param {*} req
+   * @param {*} res
+   */
+  async buscarCliente(req, res) {
+    const cliente = await Cliente.findById(req.params.id);
+
+    return res.json(cliente);
+  },
+  /**
    * Criar novo Cliente no BD
-   * @param {*} req 
-   * @param {*} res 
+   * @param {*} req
+   * @param {*} res
    */
   async criarNovoCliente(req, res) {
-      const cliente = await Cliente.create(req.body);
+    const cliente = await Cliente.create(req.body);
 
-      return res.json(cliente);
+    return res.json(cliente);
   },
 };
--- projeto-sharenergy/backend-NodeJS/src/routes.js
+++ projeto-sharenergy/backend-NodeJS/src/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const ClienteController = require("./controllers/ClienteController");
 
 routes.get("/clientes", ClienteController.listarTodosClientes);
+routes.get("/clientes/:id", ClienteController.buscarCliente);
 routes.post("/clientes", ClienteController.criarNovoCliente);
 
 module.exports = routes;
