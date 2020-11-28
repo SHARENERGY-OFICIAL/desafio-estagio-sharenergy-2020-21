835606beb38f6788017ab3314c50892b18916333
Daniel Hermenegildo <danielmarcosh@gmail.com>
2020-11-28 11:43:11 -0300

    Conexão com o Banco de Dados, dependencias instaladas:  mongoose/nodemon

--- projeto-sharenergy/backend-NodeJS/server.js
+++ projeto-sharenergy/backend-NodeJS/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require("mongoose");
+
+// Iniciando o DB
+mongoose.connect(
+    'mongodb+srv://adm:asdf-456@cluster0.ixajh.mongodb.net/sharenergy-project-db?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology: true}
+);
+
+
 app.listen(3100, () =>{
     console.log("Servidor rodando na porta 3100");
 });
