d55be87c08c93cdf360772a45b2a782207a8c3e2
Daniel Hermenegildo <danielmarcosh@gmail.com>
2020-11-29 18:54:28 -0300

    Autenticacao e Autorizacao de Usuario

--- projeto-sharenergy/backend-NodeJS/server/routes/UserRoutes.js
+++ projeto-sharenergy/backend-NodeJS/server/routes/UserRoutes.js
@@ -1,10 +1,11 @@
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middlewares/auth");
 
 module.exports = (app) => {
   app.get("/", (req, res) => {
     res.redirect("/login");
   });
-  app.get("/listarUsuarios", userController.listarUsuarios);
+  app.get("/listarUsuarios", authMiddleware, userController.listarUsuarios);
   app.post("/login", userController.login);
   app.post("/novoUsuario", userController.novoUsuario);
   app.post("/logout", userController.logout);
